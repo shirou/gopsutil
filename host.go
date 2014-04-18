4ffbd2a28ffd0e8b81ce7d420dd151c02d108a7b
WAKAYAMA Shirou <shirou.faw@gmail.com>
2014-04-18 21:27:29 +0900

    separate struct and func due to the lack of syscall on windows.

--- host.go
+++ host.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"os"
-	"syscall"
-)
-
 type Host struct{}
 
 type HostInfo struct {
@@ -16,23 +11,4 @@ type HostInfo struct {
 func NewHost() Host {
 	h := Host{}
 	return h
-}
-
-func (h Host) HostInfo() (HostInfo, error) {
-	ret := HostInfo{}
-	sysinfo := &syscall.Sysinfo_t{}
-
-	if err := syscall.Sysinfo(sysinfo); err != nil {
-		return ret, err
-	}
-
-	hostname, err := os.Hostname()
-	if err != nil {
-		return ret, err
-	}
-	ret.Hostname = hostname
-	ret.Uptime = sysinfo.Uptime
-	ret.Procs = uint64(sysinfo.Procs)
-
-	return ret, nil
 }
