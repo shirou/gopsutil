8204d224c27157d974408f78628ad9526978f4f7
WAKAYAMA Shirou <shirou.faw@gmail.com>
2014-04-20 01:53:00 +0900

    remove unused func.

--- common_windows.go
+++ common_windows.go
@@ -3,29 +3,9 @@
 package main
 
 import (
-	"fmt"
 	"syscall"
 )
 
-type Proc struct {
-	Pid uint64
-}
-
-func GetProcList() ([]Proc, error) {
-	ret := make([]Proc, 0)
-	kernel32, err := syscall.LoadLibrary("kernel32.dll")
-	if err != nil {
-		return ret, err
-	}
-	defer syscall.FreeLibrary(kernel32)
-	Process32First, _ := syscall.GetProcAddress(kernel32, "Process32First")
-
-	//	pFirst, _, err := syscall.Syscall(uintptr(Process32First), 0, 0, 0, 0)
-	pFirst, _, err := syscall.Syscall(uintptr(Process32First), 0, 0, 0, 0)
-	if err != nil {
-		return ret, err
-	}
-	fmt.Printf("Proc: %v\n", pFirst)
-	return ret, nil
-
-}
+var (
+	modKernel32 = syscall.NewLazyDLL("kernel32.dll")
+)
