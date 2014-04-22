d0da149b67121d7b2d5daf84edc8ef295a99a4c8
Shirou WAKAYAMA <shirou.faw@gmail.com>
2014-04-22 09:48:48 +0900

    implements Uptime.

--- host_unix.go
+++ host_unix.go
@@ -9,8 +9,17 @@ import (
 
 func (h Host) HostInfo() (HostInfo, error) {
 	ret := HostInfo{}
+
 	hostname, err := os.Hostname()
 	ret.Hostname = hostname
+	if err != nil {
+		return ret, err
+	}
+
+	sysinfo := &syscall.Sysinfo_t{}
+	if err := syscall.Sysinfo(sysinfo); err != nil {
+		return ret, err
+	}
 	ret.Uptime = sysinfo.Uptime
 
 	return ret, nil
