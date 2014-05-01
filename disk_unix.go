b6d9d462c2ab569c8885cecc1d41e1466222d389
Shirou WAKAYAMA <shirou.faw@gmail.com>
2014-05-01 15:47:46 +0900

    return value DiskUsageStat is now pointer

--- disk_unix.go
+++ disk_unix.go
@@ -4,16 +4,16 @@ package gopsutil
 
 import "syscall"
 
-func DiskUsage(path string) (DiskUsageStat, error) {
+func DiskUsage(path string) (*DiskUsageStat, error) {
 	stat := syscall.Statfs_t{}
 	err := syscall.Statfs(path, &stat)
 	if err != nil {
-		return DiskUsageStat{Path: path}, err
+		return nil, err
 	}
 
 	bsize := stat.Bsize / 512
 
-	ret := DiskUsageStat{
+	ret := &DiskUsageStat{
 		Path:  path,
 		Total: (uint64(stat.Blocks) * uint64(bsize)) >> 1,
 		Free:  (uint64(stat.Bfree) * uint64(bsize)) >> 1,
