bfe7890e2b96498f082eefecf777a05dcb2bda5a
WAKAYAMA shirou <shirou.faw@gmail.com>
2014-05-20 19:29:41 +0900

    fix return type.

--- load_freebsd.go
+++ load_freebsd.go
@@ -6,26 +6,26 @@ import (
 	"strconv"
 )
 
-func LoadAvg() (LoadAvgStat, error) {
+func LoadAvg() (*LoadAvgStat, error) {
 	values, err := doSysctrl("vm.loadavg")
 	if err != nil {
-		return LoadAvgStat{}, err
+		return nil, err
 	}
 
 	load1, err := strconv.ParseFloat(values[0], 64)
 	if err != nil {
-		return LoadAvgStat{}, err
+		return nil, err
 	}
 	load5, err := strconv.ParseFloat(values[1], 64)
 	if err != nil {
-		return LoadAvgStat{}, err
+		return nil, err
 	}
 	load15, err := strconv.ParseFloat(values[2], 64)
 	if err != nil {
-		return LoadAvgStat{}, err
+		return nil, err
 	}
 
-	ret := LoadAvgStat{
+	ret := &LoadAvgStat{
 		Load1:  float64(load1),
 		Load5:  float64(load5),
 		Load15: float64(load15),
