5ae4dcc8bafc6db009ff582e669438b5cb2f7967
Shirou WAKAYAMA <shirou.faw@gmail.com>
2015-10-11 22:07:18 +0900

    net[linux]: fix lsof output in linux when no pid outputed.

--- common/common_unix.go
+++ common/common_unix.go
@@ -22,11 +22,10 @@ func CallLsof(invoke Invoker, pid int32, args ...string) ([]string, error) {
 	}
 	out, err := invoke.Command(lsof, cmd...)
 	if err != nil {
-		// if no pid found, lsof returnes code 1
+		// if no pid found, lsof returnes code 1 but have output.
 		if err.Error() == "exit status 1" && len(out) == 0 {
-			return []string{}, nil
+			return []string{}, err
 		}
-		return []string{}, err
 	}
 	lines := strings.Split(string(out), "\n")
 
