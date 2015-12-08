a9e946c7dd9a830ceb183aed7f8eec35d757e56a
James Lamb <jlamb@atlassian.com>
2015-12-09 07:49:20 +1100

    change the snakecase to reflect the files being read

--- net/net.go
+++ net/net.go
@@ -65,8 +65,8 @@ type NetInterfaceStat struct {
 }
 
 type NetFilterStat struct {
-	ConnTrackCount	int64	`json:"conn_track_count"`
-	ConnTrackMax	int64	`json:"conn_track_max"`
+	ConnTrackCount	int64	`json:"conntrack_count"`
+	ConnTrackMax	int64	`json:"conntrack_max"`
 }
 
 var constMap = map[string]int{
