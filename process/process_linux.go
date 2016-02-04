6496b57b3d7e994889fd82fb5c041b428013bc6e
Robert Weber <robert.weber@logrhythm.com>
2016-02-04 15:14:11 -0700

    Need to promote this error

--- process/process_linux.go
+++ process/process_linux.go
@@ -4,6 +4,7 @@ package process
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -17,6 +18,8 @@ import (
 	"github.com/shirou/gopsutil/internal/common"
 	"github.com/shirou/gopsutil/net"
 )
+
+var ErrorNoChildren = errors.New("Process does not have children or does not exist")
 
 const (
 	PrioProcess = 0 // linux/resource.h
@@ -205,6 +208,9 @@ func (p *Process) MemoryPercent() (float32, error) {
 func (p *Process) Children() ([]*Process, error) {
 	pids, err := common.CallPgrep(invoke, p.Pid)
 	if err != nil {
+		if err == common.ErrorNoChildren {
+			return nil, ErrorNoChildren
+		}
 		return nil, err
 	}
 	ret := make([]*Process, 0, len(pids))
