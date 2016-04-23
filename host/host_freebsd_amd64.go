d9e994bec8652904d6756a1ccfe737a1a0ecb56d
WAKAYAMA shirou <shirou.faw@gmail.com>
2016-04-23 23:10:23 +0900

    [host]freebsd: update freebsd_amd64

--- host/host_freebsd_amd64.go
+++ host/host_freebsd_amd64.go
@@ -9,6 +9,7 @@ const (
 	sizeofInt      = 0x4
 	sizeofLong     = 0x8
 	sizeofLongLong = 0x8
+	sizeOfUtmpx    = 197 // TODO: why should 197, not 0x118
 )
 
 type (
@@ -24,17 +25,19 @@ type Utmp struct {
 	Host [16]int8
 	Time int32
 }
+
 type Utmpx struct {
 	Type int16
 	Tv   Timeval
-	ID   [8]int8
+	Id   [8]int8
 	Pid  int32
 	User [32]int8
 	Line [16]int8
 	Host [125]int8
-	//	Host [128]int8
-	//	X__ut_spare [64]int8
+	//      Host [128]int8
+	//      X__ut_spare [64]int8
 }
+
 type Timeval struct {
 	Sec  [4]byte
 	Usec [3]byte
