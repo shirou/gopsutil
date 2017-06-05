ff74fb36ab797c117cd0c54d33c8faa1b1cf52eb
Sean Chittenden <sean@chittenden.org>
2017-06-05 14:15:04 -0700

    Add disk IOCounters support for Illumos within Zones.

--- disk/disk_illumos.go
+++ disk/disk_illumos.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/mailru/easyjson"
 	"github.com/shirou/gopsutil/internal/common"
 	"golang.org/x/sys/unix"
 )
@@ -37,44 +38,6 @@ var (
 		"proc":   struct{}{},
 	}
 )
-
-// IOCountersZoneStat measurements are defined in
-// https://github.com/joyent/illumos-joyent/blob/master/usr/src/uts/common/sys/zone.h#L404-L422
-// however the actual members are detailed elsewhere in kstat.h:
-// https://github.com/illumos/illumos-gate/blob/master/usr/src/uts/common/sys/kstat.h#L588-L685
-//
-type IOCountersZoneStat struct {
-	Name string `json:"zonename"`
-
-	Num10msOps  uint64 `json:"10ms_ops"`  // number of IOs >=10ms
-	Num100msOps uint64 `json:"100ms_ops"` // number of IOs >= 100ms
-	Num1sOps    uint64 `json:"1s_ops"`    // number of IOs >= 1s
-	Num10sOps   uint64 `json:"10s_ops"`   // number of IOs >= 10s
-
-	// Delay* is the cumulative number of operations and time that the ZFS IO
-	// scheduler spent throttling IO for a given zone (see zone_io_delay):
-	// https://github.com/joyent/illumos-joyent/blob/master/usr/src/uts/common/fs/zfs/zfs_zone.c#L954-L995
-	// https://github.com/joyent/illumos-joyent/blob/master/usr/src/uts/common/fs/zfs/zfs_zone.c#L1046-L1063
-	DelayCount uint64        `json:"delay_cnt"`  // number of times IO has been delayed by ZFS IO scheduler
-	DelayTime  time.Duration `json:"delay_time"` // cumulative wait time IO throttled by ZFS IO scheduler
-
-	BytesRead    uint64 `json:"nread"`    // number of bytes read
-	BytesWritten uint64 `json:"nwritten"` // number of bytes written
-	NumReadOps   uint64 `json:"reads"`    // number of read operations
-	NumWriteOps  uint64 `json:"writes"`   // number of write operations
-
-	NumRunningIO  uint64        `json:"rcnt"`  // count of elements in run state
-	TimeIORunning time.Duration `json:"rtime"` // cumulative run (service) time
-
-	NumQueuedIO  uint64        `json:"wcnt"`  // count of elements in wait state
-	TimeIOQueued time.Duration `json:"wtime"` // cumulative wait (pre-service) time
-
-	// See
-	// github.com/illumos/illumos-gate/blob/master/usr/src/uts/common/sys/kstat.h#L588-L685
-	// for an explanation of these stats
-	RiemannSumRunning uint64 `json:"rlentime"` // cumulative run length*time product
-	RiemannSumQueued  uint64 `json:"wlentime"` // cumulative wait length*time product
-}
 
 func Partitions(all bool) ([]PartitionStat, error) {
 	ret := make([]PartitionStat, 0, _DEFAULT_NUM_MOUNTS)
@@ -118,7 +81,69 @@ func Partitions(all bool) ([]PartitionStat, error) {
 }
 
 func IOCounters(names ...string) (map[string]IOCountersStat, error) {
+	inZone, err := common.InZone()
+	if err != nil {
+		return nil, err
+	}
+
+	if inZone {
+		ret := make(map[string]IOCountersStat, 1)
+		ioStat, err := IOCountersZone()
+		if err != nil {
+			return nil, err
+		}
+
+		ret[ioStat.Name] = IOCountersStat{
+			ReadCount:        ioStat.NumReadOps,
+			MergedReadCount:  0,
+			WriteCount:       ioStat.NumWriteOps,
+			MergedWriteCount: 0,
+			ReadBytes:        ioStat.BytesRead,
+			WriteBytes:       ioStat.BytesWritten,
+			ReadTime:         0,
+			WriteTime:        0,
+			IopsInProgress:   ioStat.NumRunningIO + ioStat.NumQueuedIO,
+			IoTime:           uint64((ioStat.TimeIORunning + ioStat.TimeIOQueued) / time.Millisecond),
+			WeightedIO:       0,
+			Name:             ioStat.Name,
+			SerialNumber:     ioStat.Name,
+		}
+
+		return ret, nil
+	}
+
 	return nil, common.ErrNotImplementedError
+}
+
+// IOCountersZone returns the IOPS being used by the visible zone.
+func IOCountersZone() (*IOCountersZoneStat, error) {
+	kstatPath, err := common.KStatPath()
+	if err != nil {
+		return nil, fmt.Errorf("cannot find kstat(1M): %v", err)
+	}
+
+	kstatJSON, err := invoke.Command(kstatPath, "-jm", "zone_vfs")
+	if err != nil {
+		return nil, fmt.Errorf("cannot execute kstat(1M): %v", err)
+	}
+	kstatFrames := _zoneVFSKStatFrames{}
+	if err := easyjson.Unmarshal(kstatJSON, &kstatFrames); err != nil {
+		return nil, fmt.Errorf("cannot decode kstat(1M) zone_vfs JSON: %v", err)
+	}
+
+	var ret *IOCountersZoneStat
+	for _, zoneStat := range kstatFrames {
+		ret = &zoneStat.Data
+
+		// Fixup DelayTime: kstat(1M) reports this value in µs but the value is
+		// Unmarshaled as ns
+		ret.DelayTime = ret.DelayTime * time.Microsecond
+
+		// XXX Only one object for now: zones can't be nested
+		break
+	}
+
+	return ret, nil
 }
 
 func Usage(path string) (*UsageStat, error) {
