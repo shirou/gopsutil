c818f7b647e7aa817e0b82c99f90ec8973d5de21
HeChuan <tinyshrimp@163.com>
2017-07-20 11:47:15 +0800

    set win cpu total name to "cpu-total"
    set the name of windows cpu total percent to "cpu-total"
    keep the same with Linux, Freebsd system

--- cpu/cpu_windows.go
+++ cpu/cpu_windows.go
@@ -69,6 +69,7 @@ func Times(percpu bool) ([]TimesStat, error) {
 	system := (kernel - idle)
 
 	ret = append(ret, TimesStat{
+		CPU:    "cpu-total",
 		Idle:   float64(idle),
 		User:   float64(user),
 		System: float64(system),
