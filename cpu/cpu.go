10bec5ccf5ab90794cce49b48317b78c0fc4cde1
Elliot Luo <956941328@qq.com>
2019-03-20 09:39:47 +0800

    slice to string
    Signed-off-by: Elliot Luo <956941328@qq.com>

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -31,19 +31,19 @@ type TimesStat struct {
 }
 
 type InfoStat struct {
-	CPU        int32    `json:"cpu"`
-	VendorID   string   `json:"vendorId"`
-	Family     string   `json:"family"`
-	Model      string   `json:"model"`
-	Stepping   int32    `json:"stepping"`
-	PhysicalID string   `json:"physicalId"`
-	CoreID     string   `json:"coreId"`
-	Cores      int32    `json:"cores"`
-	ModelName  string   `json:"modelName"`
-	Mhz        float64  `json:"mhz"`
-	CacheSize  int32    `json:"cacheSize"`
-	Flags      []string `json:"flags"`
-	Microcode  string   `json:"microcode"`
+	CPU        int32   `json:"cpu"`
+	VendorID   string  `json:"vendorId"`
+	Family     string  `json:"family"`
+	Model      string  `json:"model"`
+	Stepping   int32   `json:"stepping"`
+	PhysicalID string  `json:"physicalId"`
+	CoreID     string  `json:"coreId"`
+	Cores      int32   `json:"cores"`
+	ModelName  string  `json:"modelName"`
+	Mhz        float64 `json:"mhz"`
+	CacheSize  int32   `json:"cacheSize"`
+	Flags      string  `json:"flags"`
+	Microcode  string  `json:"microcode"`
 }
 
 type lastPercent struct {
