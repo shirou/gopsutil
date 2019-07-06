2fd3f03f8455356d7e0034fbeed6356dc15c2499
WAKAYAMA shirou <shirou.faw@gmail.com>
2019-07-06 23:25:34 +0900

    [freebsd][host]: fix Users() return start secs.

--- host/types_freebsd.go
+++ host/types_freebsd.go
@@ -11,6 +11,7 @@ package host
 #include <sys/types.h>
 #include <sys/time.h>
 #include <utmpx.h>
+#include "freebsd_headers/utxdb.h"
 
 enum {
 	sizeofPtr = sizeof(void*),
@@ -27,7 +28,7 @@ const (
 	sizeofInt      = C.sizeof_int
 	sizeofLong     = C.sizeof_long
 	sizeofLongLong = C.sizeof_longlong
-	sizeOfUtmpx    = C.sizeof_struct_utmpx
+	sizeOfUtmpx    = C.sizeof_struct_futx
 )
 
 // Basic types
@@ -39,6 +40,5 @@ type (
 	_C_long_long C.longlong
 )
 
-type Utmp C.struct_utmp
-type Utmpx C.struct_utmpx
-type Timeval C.struct_timeval
+type Utmp C.struct_utmp // for FreeBSD 9.0 compatibility
+type Utmpx C.struct_futx
