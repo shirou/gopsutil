3743e1b5ab8d71d0bc19b7e04c2a7b56afea0783
Moises Botarro <moises.bottaro.silva@gmail.com>
2020-06-05 18:52:03 -0400

    add TestFillNSPidFromStatus unit test

--- process/process_linux_test.go
+++ process/process_linux_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	log "github.com/cihub/seelog"
+	"github.com/stretchr/testify/assert"
 )
 
 var sink map[int32]*FilledProcess
@@ -58,4 +59,31 @@ func BenchmarkLinuxAllProcessesOnLocalProcFS(b *testing.B) {
 	if errCount > 0 {
 		b.Fatal("non-zero errors for AllProcesses", errCount)
 	}
+}
+
+func TestFillNSPidFromStatus(t *testing.T) {
+	err := os.Setenv("HOST_PROC", "./resources/linux_inner_ns/proc")
+	defer os.Unsetenv("HOST_PROC")
+	assert.Nil(t, err)
+
+	// Process spawned from the host
+	// NSpid:	6320
+	p1, err := NewProcess(6320)
+	assert.Nil(t, err)
+	p1.fillFromStatus()
+	assert.Equal(t, int32(6320), p1.NSPid)
+
+	// Process spawned from P1 with its own namespace
+	// NSpid:	6321	1
+	p2, err := NewProcess(6321)
+	assert.Nil(t, err)
+	p2.fillFromStatus()
+	assert.Equal(t, int32(1), p2.NSPid)
+
+	// Process spawned from P2 with its own namespace
+	// NSpid:	6322	2	1
+	p3, err := NewProcess(6322)
+	assert.Nil(t, err)
+	p3.fillFromStatus()
+	assert.Equal(t, int32(1), p3.NSPid)
 }
