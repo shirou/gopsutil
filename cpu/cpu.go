e8a1db3c7b250b5182d18006b4b0705ec1ea2d9e
Manoj Ghosh <manoj.ghosh@oracle.com>
2020-07-10 01:05:58 -0700

    use iowait time as idle time

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -97,9 +97,9 @@ func (c InfoStat) String() string {
 }
 
 func getAllBusy(t TimesStat) (float64, float64) {
-	busy := t.User + t.System + t.Nice + t.Iowait + t.Irq +
-		t.Softirq + t.Steal
-	return busy + t.Idle, busy
+	busy := t.User + t.System + t.Nice + t.Irq +
+		t.Softirq + t.Steal + t.Guest + t.GuestNice
+	return busy + t.Idle  + t.Iowait, busy
 }
 
 func calculateBusy(t1, t2 TimesStat) float64 {
