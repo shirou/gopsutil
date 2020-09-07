8a625ec054ea912247e0372de6b2a02255a6c09b
Lomanic <Lomanic@users.noreply.github.com>
2020-09-07 21:12:17 +0200

    [host][process][darwin] Go-fmt code from mktypes.sh from #917

--- host/host_darwin_arm64.go
+++ host/host_darwin_arm64.go
@@ -6,17 +6,17 @@
 package host
 
 type Utmpx struct {
-	User	[256]int8
-	Id	[4]int8
-	Line	[32]int8
-	Pid	int32
-	Type	int16
-	Tv	Timeval
-	Host	[256]int8
-	Pad	[16]uint32
+	User [256]int8
+	Id   [4]int8
+	Line [32]int8
+	Pid  int32
+	Type int16
+	Tv   Timeval
+	Host [256]int8
+	Pad  [16]uint32
 }
 type Timeval struct {
-	Sec		int64
-	Usec		int32
-	Pad_cgo_0	[4]byte
+	Sec       int64
+	Usec      int32
+	Pad_cgo_0 [4]byte
 }
