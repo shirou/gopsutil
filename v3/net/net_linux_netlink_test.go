005a35c436616f35529a07c0b53ad6522d90a8d8
shirou <shirou.faw@gmail.com>
2020-10-26 22:24:59 +0900

    [v3 migartion] add v3 sourcodes, and change disk.opts to []string

---
+++ v3/net/net_linux_netlink_test.go
@@ -0,0 +1,18 @@
+// +build linux
+
+package net
+
+import "testing"
+
+func BenchmarkGetConnectionsInet(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Connections("inet")
+	}
+}
+func BenchmarkGetConnectionsAll(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Connections("all")
+	}
+}
