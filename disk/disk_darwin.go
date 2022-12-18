8d22915181074ba3cc954dae752a64385b57070b
shirou <shirou.faw@gmail.com>
2022-12-18 13:11:56 +0900

    [disk][darwin]: add issue URL

--- disk/disk_darwin.go
+++ disk/disk_darwin.go
@@ -27,6 +27,7 @@ func PartitionsWithContext(ctx context.Context, all bool) ([]PartitionStat, erro
 	// On 10.14, and possibly other OS versions, the actual count may
 	// be less than from the first call. Truncate to the returned count
 	// to prevent accessing uninitialized entries.
+	// https://github.com/shirou/gopsutil/issues/1390
 	fs = fs[:count]
 	for _, stat := range fs {
 		opts := []string{"rw"}
