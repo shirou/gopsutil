177e1b1982c898a01cf59f8a68d5f4bb020b8711
Antoine Toulme <antoine@lunar-ocean.com>
2023-05-26 15:58:08 -0700

    remove deprecation comments

--- internal/common/common.go
+++ internal/common/common.go
@@ -364,37 +364,30 @@ func combine(value string, combineWith []string) string {
 	}
 }
 
-// Deprecated: use HostProcWithContext instead
 func HostProc(combineWith ...string) string {
 	return GetEnv("HOST_PROC", "/proc", combineWith...)
 }
 
-// Deprecated: use HostSysWithContext instead
 func HostSys(combineWith ...string) string {
 	return GetEnv("HOST_SYS", "/sys", combineWith...)
 }
 
-// Deprecated: use HostEtcWithContext instead
 func HostEtc(combineWith ...string) string {
 	return GetEnv("HOST_ETC", "/etc", combineWith...)
 }
 
-// Deprecated: use HostVarWithContext instead
 func HostVar(combineWith ...string) string {
 	return GetEnv("HOST_VAR", "/var", combineWith...)
 }
 
-// Deprecated: use HostRunWithContext instead
 func HostRun(combineWith ...string) string {
 	return GetEnv("HOST_RUN", "/run", combineWith...)
 }
 
-// Deprecated: use HostDevWithContext instead
 func HostDev(combineWith ...string) string {
 	return GetEnv("HOST_DEV", "/dev", combineWith...)
 }
 
-// Deprecated: use HostRootWithContext instead
 func HostRoot(combineWith ...string) string {
 	return GetEnv("HOST_ROOT", "/", combineWith...)
 }
