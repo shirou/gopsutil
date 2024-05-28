aca0ba4517f781dd6a3c0d4f0c3932a149165c7c
shirou <shirou.faw@gmail.com>
2024-05-28 22:48:47 +0900

    [aix]: move tempereture from host to sensors package.

---
+++ sensors/sensors_aix.go
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//go:build aix
+
+package sensors
+
+import (
+	"context"
+
+	"github.com/shirou/gopsutil/v4/internal/common"
+)
+
+const (
+	hostTemperatureScale = 1000.0 // Not part of the linked file, but kept just in case it becomes relevant
+)
+
+func VirtualizationWithContext(ctx context.Context) (string, string, error) {
+	return "", "", common.ErrNotImplementedError
+}
--- sensors/sensors_fallback.go
+++ sensors/sensors_fallback.go
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-//go:build !darwin && !linux && !freebsd && !openbsd && !netbsd && !solaris && !windows
+//go:build !darwin && !linux && !freebsd && !openbsd && !netbsd && !solaris && !windows && !aix
 
 package sensors
 
