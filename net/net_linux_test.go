15f5f5aee3f0a593e85500eda0ad0a7e6988b242
Florian Lehner <dev@der-flo.net>
2024-05-31 11:32:19 +0200

    net/linux: use NETLINK_SOCK_DIAG
    To retrieve network information use NETLINK_SOCK_DIAG instead of walking
    /proc/<PID> and parsing files.
    Consequently this reduces the number of syscalls, as walking /proc/<PID> and
    opening, reading and closing files is no longer required. But it also reduces
    the memory footprint as reading files into memory for processing is no longer
    required.
    
    Related issues:
    - https://github.com/shirou/gopsutil/issues/695
    - https://github.com/shirou/gopsutil/issues/784
    
    Supersedes https://github.com/shirou/gopsutil/pull/809
    
    Signed-off-by: Florian Lehner <dev@der-flo.net>

--- net/net_linux_test.go
+++ net/net_linux_test.go
@@ -7,7 +7,6 @@ import (
 	"net"
 	"os"
 	"strings"
-	"syscall"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -126,66 +125,6 @@ func TestConnectionsMax(t *testing.T) {
 	}
 	for _, c := range cxByPid {
 		assert.True(t, c <= max)
-	}
-}
-
-type AddrTest struct {
-	IP    string
-	Port  int
-	Error bool
-}
-
-func TestDecodeAddress(t *testing.T) {
-	assert := assert.New(t)
-
-	addr := map[string]AddrTest{
-		"11111:0035": {
-			Error: true,
-		},
-		"0100007F:BLAH": {
-			Error: true,
-		},
-		"0085002452100113070057A13F025401:0035": {
-			IP:   "2400:8500:1301:1052:a157:7:154:23f",
-			Port: 53,
-		},
-		"00855210011307F025401:0035": {
-			Error: true,
-		},
-	}
-	if common.IsLittleEndian() {
-		addr["0500000A:0016"] = AddrTest{
-			IP:   "10.0.0.5",
-			Port: 22,
-		}
-		addr["0100007F:D1C2"] = AddrTest{
-			IP:   "127.0.0.1",
-			Port: 53698,
-		}
-	} else {
-		addr["0A000005:0016"] = AddrTest{
-			IP:   "10.0.0.5",
-			Port: 22,
-		}
-		addr["7F000001:D1C2"] = AddrTest{
-			IP:   "127.0.0.1",
-			Port: 53698,
-		}
-	}
-
-	for src, dst := range addr {
-		family := syscall.AF_INET
-		if len(src) > 13 {
-			family = syscall.AF_INET6
-		}
-		addr, err := decodeAddress(uint32(family), src)
-		if dst.Error {
-			assert.NotNil(err, src)
-		} else {
-			assert.Nil(err, src)
-			assert.Equal(dst.IP, addr.IP, src)
-			assert.Equal(dst.Port, int(addr.Port), src)
-		}
 	}
 }
 
