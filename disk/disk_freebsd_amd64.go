2758ffc346a65bc99084e20ff3c2dee4b8ce7b58
shirou <shirou.faw@gmail.com>
2025-08-26 22:46:53 +0900

    [disk][process][freebsd]: remove binary.go from common

--- disk/disk_freebsd_amd64.go
+++ disk/disk_freebsd_amd64.go
@@ -52,7 +52,7 @@ type devstat struct {
 	Device_type   uint32
 	Priority      uint32
 	Pad_cgo_1     [4]byte
-	ID            *byte
+	ID            [sizeofPtr]byte
 	Sequence1     uint32
 	Pad_cgo_2     [4]byte
 }
--- disk/disk_freebsd_arm.go
+++ disk/disk_freebsd_arm.go
@@ -50,7 +50,7 @@ type devstat struct {
 	Flags         uint32
 	Device_type   uint32
 	Priority      uint32
-	Id            *byte
+	Id            [sizeofPtr]byte
 	Sequence1     uint32
 }
 
