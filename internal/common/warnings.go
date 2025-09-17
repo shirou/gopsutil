8bd2ceb2b5f72ef5e97203c2c2063221b05ba54a
Stefano Balzarotti <stefano.balzarotti@nozominetworks.com>
2025-09-17 16:21:51 +0200

    add warnings

--- internal/common/warnings.go
+++ internal/common/warnings.go
@@ -4,14 +4,17 @@ package common
 import "fmt"
 
 const maxWarnings = 100 // An arbitrary limit to avoid excessive memory usage, it has no sense to store thousands of errors
+const tooManyErrorsMessage = "too many errors reported."
 
 type Warnings struct {
-	List    []error
-	Verbose bool
+	List          []error
+	tooManyErrors bool
+	Verbose       bool
 }
 
 func (w *Warnings) Add(err error) {
 	if len(w.List) >= maxWarnings {
+		w.tooManyErrors = true
 		return
 	}
 	w.List = append(w.List, err)
@@ -30,7 +33,13 @@ func (w *Warnings) Error() string {
 		for i, e := range w.List {
 			str += fmt.Sprintf("\tError %d: %s\n", i, e.Error())
 		}
+		if w.tooManyErrors {
+			str += fmt.Sprintf("\t%s\n", tooManyErrorsMessage)
+		}
 		return str
+	}
+	if w.tooManyErrors {
+		return tooManyErrorsMessage
 	}
 	return fmt.Sprintf("Number of warnings: %v", len(w.List))
 }
