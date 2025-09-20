aa10cf3fa6f24601954e4b08a1d53e99bca7b1c9
Stefano Balzarotti <stefano.balzarotti@nozominetworks.com>
2025-09-20 17:11:16 +0200

    gofumt

--- internal/common/warnings.go
+++ internal/common/warnings.go
@@ -3,9 +3,11 @@ package common
 
 import "fmt"
 
-const maxWarnings = 100 // An arbitrary limit to avoid excessive memory usage, it has no sense to store hundreds of errors
-const tooManyErrorsMessage = "too many errors reported, next errors were discarded"
-const numberOfWarningsMessage = "Number of warnings:"
+const (
+	maxWarnings             = 100 // An arbitrary limit to avoid excessive memory usage, it has no sense to store hundreds of errors
+	tooManyErrorsMessage    = "too many errors reported, next errors were discarded"
+	numberOfWarningsMessage = "Number of warnings:"
+)
 
 type Warnings struct {
 	List          []error
