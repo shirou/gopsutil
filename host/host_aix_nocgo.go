9d8d1869c08dd6965092295372219125bc91668f
Dylan Myers <dylan.myers@observiq.com>
2025-12-23 10:43:01 -0500

    Add AIX process support to gopsutil

--- host/host_aix_nocgo.go
+++ host/host_aix_nocgo.go
@@ -5,10 +5,20 @@ package host
 
 import (
 	"context"
-
-	"github.com/shirou/gopsutil/v4/internal/common"
+	"strconv"
+	"strings"
 )
 
-func numProcs(_ context.Context) (uint64, error) {
-	return 0, common.ErrNotImplementedError
+func numProcs(ctx context.Context) (uint64, error) {
+	out, err := invoke.CommandWithContext(ctx, "sh", "-c", "ps aux | wc -l")
+	if err != nil {
+		return 0, err
+	}
+	countStr := strings.TrimSpace(string(out))
+	count, err := strconv.ParseUint(countStr, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	// ps aux includes header line, so subtract 1 to get actual process count
+	return count - 1, nil
 }
